4f75e0290583441fc99b9fee6cf46d5a978c7b25
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-04-28 16:55:23 +0600

    Changes tailwind css color setting.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark1: '#434141',
+        dark2: '#0B0B0B',
+        blue1: '#3F90FC',
+        blue2: '#421FCF',
+        orange1: '#FFBD3E',
+        orange2: '#FF7044',
+        light1: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
